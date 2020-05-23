f04b36370934fa52591839bdde232f14adcabf06
MooseValley <Moose_Software@yahoo.com.au>
2020-05-23 11:55:15 +1000

    Add JAR and scripts

---
+++ PasswordGenerator__Create_JAR_File.bat
@@ -0,0 +1,31 @@
+REM-----------------------------
+REM PasswordGenerator
+REM-----------------------------
+echo off
+cls
+REM
+
+:STARTCOMPILE
+echo "%dirlocation%"
+del *.class
+echo Create the Manifest file:
+echo Main-Class: PasswordGenerator >MANIFEST.MF
+echo .
+
+echo Compile the Java code:
+"%dirlocation%javac.exe" PasswordGenerator.java
+
+echo .
+echo Build the JAR file:
+REM "%dirlocation%jar.exe" cvfm PasswordGenerator.jar MANIFEST.MF *.class *.png ..\icons\*.gif ..\icons\*.png  *.jpg PasswordGenerator*.txt
+"%dirlocation%jar.exe" cfm PasswordGenerator.jar MANIFEST.MF *.class PasswordGenerator*.txt
+
+REM
+del *.class
+del ..\00__common_code\*.class
+REM del *.gif
+
+:END
+echo .
+echo Finished!
+pause
